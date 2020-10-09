3644af5fe12e65b7ea5ad529914a9d04d72c0380
elazar cohen <escohen115@gmail.com>
2020-10-09 13:29:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,11 @@
+require 'pry'
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  binding.pry
+  if string.first == "s"
+    string.unshift("sssssssssss")
+
   else
     string
   end
